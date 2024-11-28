291c528627c84edb976b77254e236d80d33f3f06
alejandrodgz <alejandro_garciaz13@outlook.com>
2024-11-28 11:25:14 -0500

    new components in gifs

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'gifs-search-box',
+    templateUrl: './search-box.component.html'
+})
+
+export class SearchBoxComponent {
+ 
+}
